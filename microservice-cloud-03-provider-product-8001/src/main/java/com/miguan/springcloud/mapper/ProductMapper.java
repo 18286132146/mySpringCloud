ebf0662f736bd92235e644df829fe2fe3d66250a
18286132146 <18286132146@139.com>
2023-04-26 17:28:53 +0800

    提交

--- microservice-cloud-03-provider-product-8001/src/main/java/com/miguan/springcloud/mapper/ProductMapper.java
+++ microservice-cloud-03-provider-product-8001/src/main/java/com/miguan/springcloud/mapper/ProductMapper.java
@@ -6,6 +6,7 @@ import org.apache.ibatis.annotations.Mapper;
 import java.util.List;
 @Mapper
 public interface ProductMapper {
+
     Product findById(Long pid);
    // @Select("select pid,product_name,db_source from product")
     List<Product>findAll();
